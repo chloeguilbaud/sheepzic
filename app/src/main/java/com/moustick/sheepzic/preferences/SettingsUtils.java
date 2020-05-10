65cdaa17256aef843f552a099760a8cba64b1f8e
Mousitck <44chloeguilbaud@gmail.com>
2020-05-10 20:43:42 +0200

    feat: saving settings repository
    feat: save and get timer preferences

---
+++ app/src/main/java/com/moustick/sheepzic/preferences/SettingsUtils.java
@@ -0,0 +1,71 @@
+package com.moustick.sheepzic.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.moustick.sheepzic.utils.SerializeUtils;
+
+import java.util.ArrayList;
+
+public class SettingsUtils {
+
+    private static final String SHEEPZIC_KEY = "SHEEPZIC_KEY";
+
+    private static final String SHEEPZIC_KEY_TIMER_PREFERENCES = "SHEEPZIC_KEY_TIMER_PREFERENCES";
+
+    private static SharedPreferences getSharedPreference(Context context) {
+        return context.getSharedPreferences(SHEEPZIC_KEY, Context.MODE_PRIVATE);
+    }
+
+    private static SharedPreferences.Editor getEditor(Context context) {
+        return getSettings(context).edit();
+    }
+
+    private static SharedPreferences getSettings(Context context) {
+        return getSharedPreference(context);
+    }
+
+    private static long getLong(Context context, String key, long defValue) {
+        return getSettings(context).getLong(key, defValue);
+    }
+
+    private static void putLong(Context context, String key, long value) {
+        getEditor(context).putLong(key, value).commit();
+    }
+
+    private static String getString(Context context, String key, String defValue) {
+        return getSettings(context).getString(key, defValue);
+    }
+
+    private static void putString(Context context, String key, String value) {
+        getEditor(context).putString(key, value).commit();
+    }
+
+    private static ArrayList<String> getList(Context context, String key, ArrayList<String> defValue) {
+        return SerializeUtils.deserializeList(getSettings(context).getString(key, SerializeUtils.serialize(defValue)));
+    }
+
+    private static void putIntegerList(Context context, String key, ArrayList<Integer> value) {
+        getEditor(context).putString(key, SerializeUtils.serialize(value)).commit();
+    }
+
+    private static void remove(Context context, String key) {
+        getEditor(context).remove(key).commit();
+    }
+
+    // PUBLICS METHODS
+
+    public static void updateTimerPreferences(Context context, ArrayList<Integer> timerPreferences) {
+        putIntegerList(context, SHEEPZIC_KEY_TIMER_PREFERENCES, timerPreferences);
+    }
+
+    public static ArrayList<Integer> getTimerPreferences(Context context) {
+        ArrayList<String> timePref = getList(context, SHEEPZIC_KEY_TIMER_PREFERENCES, new ArrayList<>());
+        ArrayList<Integer> tmp = new ArrayList<>();
+        for (String str : timePref)
+            tmp.add(Integer.parseInt(str));
+        return tmp;
+    }
+
+
+}
---
+++ app/src/main/java/com/moustick/sheepzic/utils/SerializeUtils.java
@@ -0,0 +1,34 @@
+package com.moustick.sheepzic.utils;
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializeUtils {
+
+    public static String serialize(@Nullable ArrayList list) {
+        Gson gson = new Gson();
+        if (list != null) {
+            return gson.toJson(list);
+        } else {
+            return gson.toJson(new ArrayList());
+        }
+    }
+
+    public static ArrayList<String> deserializeList(@Nullable String serializedList) {
+        Gson gson = new Gson();
+        if (serializedList == null || serializedList.isEmpty()) {
+            return new ArrayList<>();
+        } else {
+            Type type = new TypeToken<List<String>>() {
+            }.getType();
+            return gson.fromJson(serializedList, type);
+        }
+    }
+
+}
